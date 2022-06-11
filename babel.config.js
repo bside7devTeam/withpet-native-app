b2d31096432f62e5ed383035e412c4cc578df39e
JaeYoung Park <zeppy6213@gmail.com>
2022-06-11 13:48:15 +0900

    default setting

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,33 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '~': './src',
+          '@components': './src/components',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@assets': './src/assets',
+          '@api': './src/api',
+          '@helper': './src/helper',
+          '@ui': './src/ui',
+          '@store': './src/store',
+        },
+      },
+    ],
+  ],
 };
